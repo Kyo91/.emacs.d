1ee3730e1d1865c7f194fb6618d85b393430f83b
Sam Blumenthal <sam@localhost.localdomain>
2016-01-21 22:31:42 -0500

    Fixed some installs for a new system.

---
+++ elpa/flx-20151030.1112/flx-autoloads.el
@@ -0,0 +1,15 @@
+;;; flx-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("flx.el") (22177 38331 929450 584000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; flx-autoloads.el ends here
---
+++ elpa/rtags-20160114.1114/rtags-pkg.el
@@ -0,0 +1,4 @@
+(define-package "rtags" "20160114.1114" "A front-end for rtags" 'nil :url "http://rtags.net")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
